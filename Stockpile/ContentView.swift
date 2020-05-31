ad19fd13aa93960938fe18ddf6f03292fc800cdf
taylor-young0 <59883690+taylor-young0@users.noreply.github.com>
2020-05-31 17:32:07 -0400

    Initial Commit

---
+++ Stockpile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stockpile
+//
+//  Created by Taylor Young on 2020-05-31.
+//  Copyright © 2020 Taylor Young. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
