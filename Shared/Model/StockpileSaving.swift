2ace984808681ea81ab0f09e3de0a696264f2c67
Taylor Young <59883690+taylor-young0@users.noreply.github.com>
2023-01-13 21:46:06 -0500

    Move StockpileSaving samples to their own file

--- Shared/Model/StockpileSaving.swift
+++ Shared/Model/StockpileSaving.swift
@@ -31,54 +31,6 @@ public class StockpileSaving: NSManagedObject, Identifiable {
     var percentageSavings: Double {
         return (((regularPrice - salePrice) / regularPrice) * 100)
     }
-    
-    static var sample: StockpileSaving {
-        let stockpile = StockpileSaving()
-        stockpile.consumption = 0.5
-        stockpile.consumptionUnit = ConsumptionUnit.Week.rawValue
-        stockpile.dateComputed = Date()
-        stockpile.productDescription = "🧀 Organic Meadow cheese"
-        stockpile.productExpiryDate = Date.distantFuture
-        stockpile.regularPrice = 9.99
-        stockpile.salePrice = 7.49
-        stockpile.unitsPurchased = 4
-        
-        return stockpile
-    }
-
-    static var samples: [StockpileSaving] {
-        let stockpile = StockpileSaving()
-        stockpile.consumption = 0.5
-        stockpile.consumptionUnit = ConsumptionUnit.Week.rawValue
-        stockpile.dateComputed = Date()
-        stockpile.productDescription = "🧀 Organic Meadow cheese"
-        stockpile.productExpiryDate = Date.distantFuture
-        stockpile.regularPrice = 9.99
-        stockpile.salePrice = 7.49
-        stockpile.unitsPurchased = 4
-
-        let stockpileTwo = StockpileSaving()
-        stockpileTwo.consumption = 1
-        stockpileTwo.consumptionUnit = ConsumptionUnit.Month.rawValue
-        stockpileTwo.dateComputed = Date()
-        stockpileTwo.productDescription = "🍕 Amy's vegan margherita frozen pizza"
-        stockpileTwo.productExpiryDate = Date.distantFuture
-        stockpileTwo.regularPrice = 12.99
-        stockpileTwo.salePrice = 9.99
-        stockpileTwo.unitsPurchased = 2
-
-        let stockpileThree = StockpileSaving()
-        stockpileThree.consumption = 2
-        stockpileThree.consumptionUnit = ConsumptionUnit.Week.rawValue
-        stockpileThree.dateComputed = Date()
-        stockpileThree.productDescription = "🧃 Kiju organic apple juice 1L"
-        stockpileThree.productExpiryDate = Date.distantFuture
-        stockpileThree.regularPrice = 4.99
-        stockpileThree.salePrice = 3.49
-        stockpileThree.unitsPurchased = 6
-
-        return [stockpile, stockpileTwo, stockpileThree]
-    }
 }
 
 extension StockpileSaving {
